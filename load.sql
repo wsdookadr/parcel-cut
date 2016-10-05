c1ea3f9927a2dca14d7bd1e4f588c9394b1a2f10
Stefan Corneliu Petrea <stefan@garage-coding.com>
2016-10-05 12:07:51 +0300

    refactored loading script

--- load.sql
+++ load.sql
@@ -1,7 +1,49 @@
--- get a park from the OSM data to use as a test polygon.
--- we'll then run the algorithm on it (to cut pieces from it).
-\copy (SELECT name, ST_Transform(way,4326) FROM planet_osm_polygon WHERE tags->'leisure'='park' AND name LIKE '%Herăstrău%') TO '/tmp/parcels.copy' DELIMITER ',' CSV HEADER;
+-- get a some polygons from the OSM data we'll use those for tests.
+-- we also need to get some nearby roads.
+-- we'll then run the algorithm on that data(to cut pieces from it).
 SET search_path = public, plan;
+CREATE TEMP VIEW parcels_v AS (
+    SELECT
+    name, way
+    FROM planet_osm_polygon
+    WHERE tags->'leisure'='park'
+    AND name LIKE '%Herăstrău%'
+    ORDER BY ST_Area(way) DESC
+    LIMIT 4
+);
+
+CREATE TEMP VIEW roads_v AS (
+    WITH roads AS (
+        SELECT
+        osm_id, name, way
+        FROM planet_osm_line
+        WHERE tags->highway = 'motorway'
+    )
+    -- do KNN cross-join between parcels and roads
+    -- to get nearby roads (rn)
+    SELECT
+    DISTINCT ON (rn.osm_id)
+    rn.name, rn.way
+    FROM parcels_v p
+    CROSS JOIN LATERAL (
+        -- get the 5 closest roads to each parcel
+        SELECT
+        *,
+        -- distance to road
+        ST_Distance(p.way,r.way) as dr
+        FROM roads r
+        ORDER BY p.way <-> r.way
+        LIMIT 5
+    ) rn
+);
+
+SET search_path = public, pg_catalog;
+\copy (SELECT name, ST_Transform(way,4326) FROM parcels_v) TO '/tmp/parcels.copy' DELIMITER ',' CSV HEADER;
+\copy (SELECT name, ST_Transform(way,4326) FROM   roads_v) TO '/tmp/roads.copy'   DELIMITER ',' CSV HEADER;
+
+SET search_path = public, plan;
+TRUNCATE parcel RESTART IDENTITY;
+TRUNCATE road   RESTART IDENTITY;
 \copy parcel (name, way) FROM '/tmp/parcels.copy' DELIMITER ',' CSV HEADER;
--- TODO: need to have road samples here
+\copy parcel (name, way) FROM '/tmp/parcels.copy' DELIMITER ',' CSV HEADER;
 
