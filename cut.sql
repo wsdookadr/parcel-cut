e10799955a27bed2f6eda0b0ccd54f1beb9d7277
Stefan Corneliu Petrea <stefan@garage-coding.com>
2016-10-05 10:02:14 +0300

    more work on getting nearby roads for sample data

--- cut.sql
+++ cut.sql
@@ -1,5 +1,6 @@
--- this block will make some settings and
--- supress unneeded output 
+-- this block will set some flags, and supress unneeded output
+-- from the output. this is the workbench program where most development
+-- will be carried out and we want to output an SVG from here.
 \set QUIET 1
 \a
 -- turn off footer
@@ -11,25 +12,44 @@
 SET search_path = public, plan;
 \set QUIET 0
 
-WITH a AS (
-    SELECT
-    name, way
-    FROM parcel
-    LIMIT 1
-), d AS (
-    SELECT
-    ST_Envelope(ST_Collect(way)) env
-    FROM a
-), c AS (
-    SELECT
-    ST_Centroid(env) AS w
-    FROM d
-)
-SELECT
-'<html><svg width="100%" height="100%" preserveAspectRatio="" viewBox="' ||
-concat_ws(' ', ST_XMin(d.env), ST_YMax(d.env) * -1, (ST_XMax(d.env) - ST_XMin(d.env)), (ST_YMax(d.env) - ST_YMin(d.env))) || '">' ||
-'<path fill="wheat" stroke="red" stroke-width="0.00005" d="' || ST_AsSVG(a.way) || '"/>' ||
-'</svg></html>'
-FROM a,d;
+\set QUIET 1
+DROP FUNCTION parcels_draw();
+CREATE OR REPLACE FUNCTION parcels_draw() RETURNS text AS $$
+DECLARE
+retval text;
+BEGIN
+    retval := (
+        WITH a AS (
+            SELECT
+            name, way
+            FROM parcel
+            LIMIT 1
+        ), d AS (
+            SELECT
+            ST_Envelope(ST_Collect(way)) env
+            FROM a
+        ), c AS (
+            SELECT
+            ST_Centroid(env) AS w
+            FROM d
+        ), svg AS (
+            SELECT
+            (
+            '<html><svg width="100%" height="100%" preserveAspectRatio="" viewBox="' ||
+            concat_ws(' ', ST_XMin(d.env), ST_YMax(d.env) * -1, (ST_XMax(d.env) - ST_XMin(d.env)), (ST_YMax(d.env) - ST_YMin(d.env))) || '">' ||
+            '<path fill="wheat" stroke="red" stroke-width="0.00005" d="' || ST_AsSVG(a.way) || '"/>' ||
+            '</svg></html>'
+            ) AS content
+            FROM a,d
+        )
+        SELECT content
+        FROM svg
+    );
 
+    RETURN retval;
+END;
+$$ LANGUAGE plpgsql;
+\set QUIET 0
+
+SELECT parcels_draw();
 
--- load.sql
+++ load.sql
@@ -1,5 +1,7 @@
 -- get a park from the OSM data to use as a test polygon.
 -- we'll then run the algorithm on it (to cut pieces from it).
-\copy (SELECT name, ST_Transform(way,4326) FROM planet_osm_polygon WHERE tags->'leisure'='park' AND name LIKE '%Herăstrău%') TO '/tmp/data.copy' DELIMITER ',' CSV HEADER;
+\copy (SELECT name, ST_Transform(way,4326) FROM planet_osm_polygon WHERE tags->'leisure'='park' AND name LIKE '%Herăstrău%') TO '/tmp/parcels.copy' DELIMITER ',' CSV HEADER;
 SET search_path = public, plan;
-\copy parcel (name, way) FROM '/tmp/data.copy' DELIMITER ',' CSV HEADER;
+\copy parcel (name, way) FROM '/tmp/parcels.copy' DELIMITER ',' CSV HEADER;
+-- TODO: need to have road samples here
+
